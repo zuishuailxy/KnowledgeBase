b9bde29e7385e9180531f2dfcbf50f6b5d4d5798
lixy115 <zuishuailxy@gmail.com>
2025-05-30 16:59:25 +0800

    vue3 defineModal 使用

---
+++ Projects/vue3/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Cards from './components/Card/Cards.vue';
+// import Users from './components/User/Users.vue';
+// import Users from './components/User2/Users.vue';
+import UseCounter from './components/Counter/UseCounter.vue';
+
+</script>
+
+<template>
+  <!-- <Cards /> -->
+  <UseCounter />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
