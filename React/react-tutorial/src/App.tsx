bcb0fca34e315243221e8120b3ff4980f454cffa
lixy115 <zuishuailxy@gmail.com>
2025-11-28 20:49:30 +0800

    Refactor cart item rendering into CartItem component
    Moved cart item row rendering logic from App to a new CartItem component for better readability and maintainability. Updated reducer logic to use immutable state updates for all actions.

--- React/react-tutorial/src/App.tsx
+++ React/react-tutorial/src/App.tsx
@@ -13,26 +13,79 @@ const reducer = (
   state: Data,
   action: { type: "increment" | "decrement" | "edit" | "delete" | "update_name"  ; id: number, newName?: string }
 ) => {
-  const item = state.find((item) => item.id === action.id)!;
   switch (action.type) {
     case "increment":
-      item.count++;
-      return [...state];
+      return state.map(item =>
+        item.id === action.id ? { ...item, count: item.count + 1 } : item
+      );
     case "decrement":
-      item.count--;
-      return [...state];
+      return state.map(item =>
+        item.id === action.id ? { ...item, count: Math.max(1, item.count - 1) } : item
+      );
     case "delete":
-      return state.filter((item) => item.id !== action.id);
+      return state.filter(item => item.id !== action.id);
     case "edit":
-      item.isEdit = !item.isEdit;
-      return [...state];
+      return state.map(item =>
+        item.id === action.id ? { ...item, isEdit: !item.isEdit } : item
+      );
     case "update_name":
-      item.name = action.newName!;
-      return [...state];
+      return state.map(item =>
+        item.id === action.id ? { ...item, name: action.newName! } : item
+      );
     default:
       return state;
   }
 };
+
+function CartItem({
+  item,
+  dispatch,
+}: {
+  item: typeof initData[number];
+  dispatch: React.Dispatch<any>;
+}) {
+  return (
+    <tr key={item.id}>
+      <td align="center">
+        {item.isEdit ? (
+          <input
+            onBlur={() => dispatch({ type: "edit", id: item.id })}
+            onChange={(e) =>
+              dispatch({
+                type: "update_name",
+                id: item.id,
+                newName: e.target.value,
+              })
+            }
+            type="text"
+            value={item.name}
+          />
+        ) : (
+          item.name
+        )}
+      </td>
+      <td align="center">{item.price}</td>
+      <td align="center">
+        <button onClick={() => dispatch({ type: "increment", id: item.id })}>
+          +
+        </button>
+        {item.count}
+        <button onClick={() => dispatch({ type: "decrement", id: item.id })}>
+          -
+        </button>
+      </td>
+      <td align="center">{item.price * item.count}</td>
+      <td align="center">
+        <button onClick={() => dispatch({ type: "edit", id: item.id })}>
+          edit
+        </button>
+        <button onClick={() => dispatch({ type: "delete", id: item.id })}>
+          delete
+        </button>
+      </td>
+    </tr>
+  );
+}
 
 function App() {
   const [data, dispatch] = useReducer(reducer, initData);
@@ -51,45 +104,7 @@ function App() {
         </thead>
         <tbody>
           {data.map((item) => (
-            <tr key={item.id}>
-              <td align="center">
-                {item.isEdit ? (
-                  <input onBlur={() => dispatch({ type: "edit", id: item.id })} onChange={(e)=>dispatch({
-                    type:"update_name",
-                    id: item.id,
-                    newName: e.target.value
-                  })} type="text" value={item.name} />
-                ) : (
-                  item.name
-                )}
-              </td>
-              <td align="center">{item.price}</td>
-              <td align="center">
-                <button
-                  onClick={() => dispatch({ type: "increment", id: item.id })}
-                >
-                  +
-                </button>
-                {item.count}
-                <button
-                  onClick={() => dispatch({ type: "decrement", id: item.id })}
-                >
-                  -
-                </button>
-              </td>
-
-              <td align="center">{item.price * item.count}</td>
-              <td align="center">
-                <button onClick={() => dispatch({ type: "edit", id: item.id })}>
-                  edit
-                </button>
-                <button
-                  onClick={() => dispatch({ type: "delete", id: item.id })}
-                >
-                  delete
-                </button>
-              </td>
-            </tr>
+            <CartItem key={item.id} item={item} dispatch={dispatch} />
           ))}
         </tbody>
         <tfoot>
