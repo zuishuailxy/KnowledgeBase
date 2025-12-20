6417357e7c62683646b51189c5f9047fc8826d06
zuishuailxy <zuishuailxy@gmail.com>
2025-12-20 12:00:49 +0800

    Refactor router and Home page imports
    Updated import paths in Home.tsx and router/index.ts to use absolute imports for components. This improves code readability and maintainability by standardizing import statements.

--- React/react-tutorial/src/pages/Home.tsx
+++ React/react-tutorial/src/pages/Home.tsx
@@ -1,7 +1,56 @@
 import { NavLink } from "react-router";
+import { Card, Form, Input, Button } from "antd";
+import {
+  useLoaderData,
+  useSubmit,
+  useNavigation,
+  useActionData,
+} from "react-router";
 
 const Home = () => {
-  return <div><NavLink to="/about">About</NavLink></div>;
+  const data = useLoaderData() as {
+    data: { name: string; age: number }[];
+    success: boolean;
+  };
+  const submit = useSubmit();
+  const onFinish = (values: any) => {
+    console.log("Form values:", values);
+    submit(values, { method: "post", encType: "application/json" });
+  };
+
+  const navigation = useNavigation();
+  const actionData = useActionData() as { success: boolean } | undefined;
+  console.log("actionData", actionData);
+
+  return (
+    <Card>
+      <h1>Welcome to the Home Page</h1>
+      <Form onFinish={onFinish}>
+        <Form.Item label="Username" name="username">
+          <Input placeholder="Enter your username" />
+        </Form.Item>
+        <Form.Item label="Age" name="age">
+          <Input placeholder="Enter your age" />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            loading={navigation.state === "submitting"}
+            type="primary"
+            htmlType="submit"
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <ul>
+        {data.data.map((item) => (
+          <li key={item.name}>
+            {item.name}: {item.age}
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
 };
 
 export default Home;
--- React/react-tutorial/src/router/index.ts
+++ React/react-tutorial/src/router/index.ts
@@ -2,15 +2,37 @@ import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
 import About from "../pages/About";
 
+const data = [
+  { name: "leo", age: 18 },
+  { name: "lily", age: 20 },
+  { name: "lucy", age: 22 },
+];
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+    loader: async () => {
+      await sleep(1000);
+      return {
+        data,
+        success: true,
+      };
+    },
+    action: async ({ request }) => {
+      const jsonData = await request.json();
+      // const jsonData = Object.fromEntries(formData.entries());
+      console.log("Submitted data:", jsonData);
+      await sleep(1000);
+      data.push({ name: jsonData.username, age: Number(jsonData.age) });
+      return { success: true };
+    },
   },
   {
     path: "/about",
     Component: About,
-  }
-])
+  },
+]);
 
 export default router;
